10e1b2aa6b6af103921c26625a254668ad239e1d
Wouter Lievens <lievenswouter@gmail.com>
2019-05-26 20:57:05 +0200

    using prognoses numbers, ui in dutch

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -15,6 +15,8 @@ export class MainPageComponent implements OnInit {
 
   coalitions: Coalition[];
 
+  segmented: boolean = true;
+
   constructor(
     private coalitionCalculatorService: CoalitionCalculatorService
   ) {
@@ -22,34 +24,45 @@ export class MainPageComponent implements OnInit {
 
   ngOnInit() {
     const parties = [
-      /*  0 */ new Party('VL', 'N-VA', '#eaf629', 33),
-      /*  1 */ new Party('WA', 'PS', '#b42a4b', 23),
-      /*  2 */ new Party('WA', 'MR', '#3e5e9d', 20),
-      /*  3 */ new Party('VL', 'CD&V', '#ffa43c', 18),
-      /*  4 */ new Party('VL', 'Open Vld', '#47749d', 14),
-      /*  5 */ new Party('VL', 'sp.a', '#a2292b', 13),
-      /*  6 */ new Party('WA', 'cdH', '#d16f33', 9),
-      /*  7 */ new Party('WA', 'Ecolo', '#71cb6c', 6),
-      /*  8 */ new Party('VL', 'Groen', '#379c4b', 6),
-      /*  9 */ new Party('VL', 'VB', '#5e5552', 3, true),
-      /* 10 */ new Party('WA', 'FDF', '#3794c0', 2),
-      /* 11 */ new Party('WA', 'PTB / PvdA', '#641919', 2),
-      /* 12 */ new Party('WA', 'PP', '#615d6e', 1, true)
+      /*  0 */ new Party('VL', 'N-VA', '#d8d326', 25),
+      /*  1 */ new Party('WA', 'PS', '#dc3a26', 20),
+      /*  2 */ new Party('VL', 'VB', '#766a2d', 18),
+      /*  3 */ new Party('WA', 'MR', '#3e5e9d', 14),
+      /*  4 */ new Party('WA', 'Ecolo', '#71cb6c', 13),
+      /*  5 */ new Party('VL', 'Open Vld', '#47749d', 12),
+      /*  6 */ new Party('VL', 'CD&V', '#ffa43c', 11),
+      /*  7 */ new Party('VL', 'sp.a', '#cd3436', 9),
+      /*  8 */ new Party('VL', 'Groen', '#379c4b', 9),
+      /*  9 */ new Party('WA', 'PTB', '#641919', 7),
+      /* 10 */ new Party('WA', 'cdH', '#d16f33', 5),
+      /* 11 */ new Party('VL', 'PVDA', '#641919', 5),
+      /* 12 */ new Party('WA', 'Défi', '#3794c0', 2),
     ];
 
     const exclusions = [
-      [parties[0], parties[10]],
-      [parties[0], parties[11]]
+      [parties[2]],
+      [parties[0], parties[9]],
+      [parties[0], parties[11]],
+      [parties[0], parties[12]],
+      [parties[6], parties[9]],
+      [parties[6], parties[11]],
     ];
 
     const pairs = [
-      [parties[7], parties[8]]
+      [parties[5], parties[8]],
+      [parties[9], parties[11]],
     ];
+
+    parties.sort((a, b) => b.seats - a.seats);
 
     this.parties = parties;
     this.exclusions = exclusions;
     this.pairs = pairs;
 
-    this.coalitions = this.coalitionCalculatorService.calculateCoalitions(parties, this.exclusions, this.pairs);
+    this.calculateCoalitions();
+  }
+
+  calculateCoalitions() {
+    this.coalitions = this.coalitionCalculatorService.calculateCoalitions(this.parties, this.exclusions, this.pairs, 0, true, this.segmented);
   }
 }
